696e862ebf2ce36802cfb43103a8f3ed209dda51
Mariano Molina <mariano.molina@salesforce.com>
2022-12-05 13:41:25 -0300

    change port

--- apollo-subgraph/index.js
+++ apollo-subgraph/index.js
@@ -82,6 +82,6 @@ const server = new ApolloServer({
 });
 
 // The `listen` method launches a web server.
-server.listen(80).then(({ url }) => {
+server.listen(8080).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
